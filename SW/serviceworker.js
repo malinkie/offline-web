7fc18b8c4d93d8e445b27809a0f2dd544b8d408e
smorrice <smorrice@adobe.com>
2016-02-13 14:39:00 -0800

    remove Promise reference coz esplosion

--- SW/serviceworker.js
+++ SW/serviceworker.js
@@ -28,22 +28,3 @@ self.addEventListener('fetch', function(event) {
             })
         );
     });
-
-self.addEventListener('activate', function(event) {
- 
-  // Array of cache that we will use in this version
-  var cacheWhitelist = ['pages-cache-v1', 'blog-posts-cache-v1'];
-
-  event.waitUntil(
-    caches.keys().then(function(cacheNames) {
-      return Promise.all(
-        cacheNames.map(function(cacheName) {
-          if (cacheWhitelist.indexOf(cacheName) === -1) {
-            // Deletes the cache because we won't use it here
-            return caches.delete(cacheName);
-          }
-        })
-      );
-    })
-  );
-});
